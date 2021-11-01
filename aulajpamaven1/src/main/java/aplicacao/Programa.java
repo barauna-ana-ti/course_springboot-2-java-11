febb13ae146bf28dacac5f96f5adbb7c1cf8785d
barauna-ana-ti <barauna.ana.ti@gmail.com>
2021-11-01 18:32:15 -0200

    Project created

---
+++ aulajpamaven1/src/main/java/aplicacao/Programa.java
@@ -0,0 +1,51 @@
+package aplicacao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import dominio.Pessoa;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		/** Retirados após cadatro no banco
+		Pessoa p1 = new Pessoa (null, "Carlos da Silva", "carlos@gmail.com");
+		Pessoa p2 = new Pessoa (null, "Joaquim Torres", "joaquim@gmail.com");
+		Pessoa p3 = new Pessoa (null, "Ana Maria", "ana@gmail.com");
+		System.out.println(p1);
+		System.out.println(p2);
+		System.out.println(p3);
+		*/
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+		EntityManager em = emf.createEntityManager();
+		
+		/** Retirado apos demonstracao de transação
+		em.getTransaction().begin();
+		em.persist(p1);
+		em.persist(p2);
+		em.persist(p3);
+		em.getTransaction().commit();
+		*/
+		
+		/** Retirado apos demonstracao de consulta
+		Pessoa p = em.find(Pessoa.class,2);
+		System.out.println(p);
+		*/
+		
+		/** Não funciona pois a entidade deve estar monitorada
+		Pessoa p = new Pessoa(2,null,null);
+		em.remove(p);		
+		*/
+		
+		
+		Pessoa p = em.find(Pessoa.class,2);
+		em.getTransaction().begin();
+		em.remove(p);
+		em.getTransaction().commit();
+		System.out.println("Pronto!");
+		em.close();
+		emf.close();
+		
+	}
+}
